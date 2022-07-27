d03ca12948a11de3dbaf1a14a1fe997d37291385
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2022-07-27 17:38:01 +0800

    Add game point for game loop

---
+++ projects/Rock-Paper-Scissors/script_rps.js
@@ -0,0 +1,21 @@
+//Game Point
+let gamePoint;
+
+//Poitns for ENEMY and USER
+let enemyPoint = 0,
+  userPoint = 0;
+
+//Get Game Point
+while (isNaN(gamePoint)) {
+  gamePoint = prompt("Game Point:");
+}
+
+const getUserChoice = () => {};
+
+const getEnemyPoint = () => {};
+
+//Game Loop
+while (true) {
+  if (enemyPoint >= gamePoint || userPoint >= gamePoint) break;
+  let userPoint;
+}
