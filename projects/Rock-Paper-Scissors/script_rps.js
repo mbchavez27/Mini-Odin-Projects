045d539c3d89ead432d710356f25e597c5c2216a
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2022-07-27 17:58:52 +0800

    Able to choose for user and choose randmoly for cpu

--- projects/Rock-Paper-Scissors/script_rps.js
+++ projects/Rock-Paper-Scissors/script_rps.js
@@ -1,3 +1,6 @@
+//Choices
+const CHOICES = ["ROCK", "PAPER", "SCISSORS"];
+
 //Game Point
 let gamePoint;
 
@@ -10,12 +13,29 @@ while (isNaN(gamePoint)) {
   gamePoint = prompt("Game Point:");
 }
 
-const getUserChoice = () => {};
+const getUserChoice = () => {
+  while (true) {
+    let userChoice = prompt(
+      "What is your move? \r\nRock\r\nPaper\r\nScissors"
+    ).toUpperCase();
+    return userChoice;
+  }
+};
 
-const getEnemyPoint = () => {};
+const getCPUChoice = () => {
+  let randomChoice = Math.floor(Math.random() * 3);
+  return CHOICES[randomChoice];
+};
+
+const checkWinner = (userChoice, cpuChoice) => {};
 
 //Game Loop
-while (true) {
-  if (enemyPoint >= gamePoint || userPoint >= gamePoint) break;
-  let userPoint;
-}
+// while (true) {
+// if (enemyPoint >= gamePoint || userPoint >= gamePoint) break;
+let userChoice = getUserChoice();
+console.log(`You chose ${userChoice}`);
+let cpuChoice = getCPUChoice();
+console.log(`CPU chose ${cpuChoice}`);
+
+checkWinner(userChoice, cpuChoice);
+// }
