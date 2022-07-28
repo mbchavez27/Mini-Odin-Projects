8493fb980276b47fe1a3bcac05b0885ede2ad7d2
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2022-07-28 21:34:28 +0800

    Add feature to set game point before starting the game

--- projects/Rock-Paper-Scissors/script_rps.js
+++ projects/Rock-Paper-Scissors/script_rps.js
@@ -1,1 +1,11 @@
+let gamePoint = 1;
+let startGameButton = document.querySelector("#startGame");
+let startScreen = document.querySelector(".startScreen");
+let setGamePoint = document.querySelector("#gamePoint");
 
+startGameButton.addEventListener("click", () => {
+  startScreen.style.visibility = "hidden";
+  gamePoint = setGamePoint.value;
+  console.log(gamePoint);
+  return gamePoint;
+});
