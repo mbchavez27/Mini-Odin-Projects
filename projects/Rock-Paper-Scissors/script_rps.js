14415b4efbeb381d2e9ebbdf7ba091c148eaf10f
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2022-07-28 21:58:04 +0800

    Able to change screen when starting the game

--- projects/Rock-Paper-Scissors/script_rps.js
+++ projects/Rock-Paper-Scissors/script_rps.js
@@ -2,10 +2,12 @@ let gamePoint = 1;
 let startGameButton = document.querySelector("#startGame");
 let startScreen = document.querySelector(".startScreen");
 let setGamePoint = document.querySelector("#gamePoint");
+let gameScreen = document.querySelector(".gameScreen");
 
 startGameButton.addEventListener("click", () => {
-  startScreen.style.visibility = "hidden";
+  startScreen.style.display = "none";
   gamePoint = setGamePoint.value;
   console.log(gamePoint);
+  gameScreen.style.display = "flex";
   return gamePoint;
 });
