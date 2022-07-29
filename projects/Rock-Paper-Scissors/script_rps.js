06af6dd4589341d4e952f60b7083540e8a5c0578
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2022-07-29 13:21:39 +0800

    Add animation

--- projects/Rock-Paper-Scissors/script_rps.js
+++ projects/Rock-Paper-Scissors/script_rps.js
@@ -1,3 +1,4 @@
+//To set Game Point
 let gamePoint = 1;
 let startGameButton = document.querySelector("#startGame");
 let startScreen = document.querySelector(".startScreen");
@@ -11,3 +12,24 @@ startGameButton.addEventListener("click", () => {
   gameScreen.style.display = "flex";
   return gamePoint;
 });
+
+let gameStatus = document.querySelector("#gameStatus");
+
+function changeGameStatus(isPlayerTurn) {
+  //If isPLayerTurn is true then game status will be players turn else be cpu's turn
+  if (isPlayerTurn) return (gameStatus.innerHTML = "Player's Turn");
+  else return (gameStatus.innerHTML = "CPU's Turn");
+}
+
+const move = (moveType) => {
+  moveType.moveReference.classList.add(moveType.moveSide);
+  return moveType.moveName;
+};
+
+const Rock = {
+  moveReference: document.querySelector("#pRock"),
+  moveName: "ROCK",
+  moveSide: "moveRight",
+};
+
+document.querySelector("#pRock").addEventListener("click", move(e, Rock));
