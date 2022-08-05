ee89c34dc4e09e2cb7455500f0ae08663a2d9b4c
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2022-08-05 15:59:40 +0800

    Create changeable grid

--- projects/Etch-A-Sketch/script_etch.js
+++ projects/Etch-A-Sketch/script_etch.js
@@ -1,5 +1,10 @@
+//Container of the Grid
 const gridContainer = document.querySelector(".gridContainer");
-let gridSize = 16;
+//To Change Grid Size
+let gridRange = document.querySelector("#gridSize");
+let gridSize = gridRange.value;
+//To Change Grid Color
+let color = "black";
 
 function addGridBox(row) {
   for (let i = 0; i < gridSize; i++) {
@@ -7,9 +12,31 @@ function addGridBox(row) {
     gridBox.classList.add("gridBox");
     gridContainer.appendChild(row);
     row.appendChild(gridBox);
+
+    gridBox.addEventListener("dragenter", () => {
+      gridBox.style.backgroundColor = color;
+    });
   }
 }
-for (let i = 0; i < gridSize; i++) {
-  let row = document.createElement("div");
-  addGridBox(row);
+
+function addGrid() {
+  for (let i = 0; i < gridSize; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    addGridBox(row);
+  }
 }
+
+function deleteGrid() {
+  let rows = document.querySelectorAll(".row");
+  rows.forEach((row) => row.remove());
+}
+
+addGrid();
+
+gridRange.addEventListener("change", () => {
+  gridSize = gridRange.value;
+  deleteGrid();
+  addGrid();
+  console.log(rows);
+});
